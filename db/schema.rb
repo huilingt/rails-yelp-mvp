877906a5f06341128a8ab9d019a1fe9b0167311b
Huiling Tan <huilingt@hotmail.com>
2022-05-26 13:07:28 +0800

    Add restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_26_031901) do
+ActiveRecord::Schema.define(version: 2022_05_26_034035) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "address"
+    t.string "phone_number"
     t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.integer "restaurant_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
